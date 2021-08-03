6b009b460ddcd8f4c3d384a9feed2879f27ca97d
lucasjord <lucas_hyland@hotmail.com>
2021-08-03 13:54:14 +1000

    fixed error in distribute

--- distribute.sh
+++ distribute.sh
@@ -2,10 +2,14 @@
 
 git pull 
 
-export class=$(sed 's/.$//' $1)
+export class=$(echo $! | sed 's/.$//')
+
+echo "Newsmerd for slogit"
 rsync -Puv /home/observer/correlations2/spiralssched/vex/$1.vex observer@newsmerd:/vlbobs/lba/$class/$1.vex
+echo "Hobart12"
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfshb:~/
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfs-2hb:/usr2/sched/
+echo "Katherine"
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfske:~/
-scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfs-2ke:~/
+scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfs-2ke:/usr2/sched/
 
