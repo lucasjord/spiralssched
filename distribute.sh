818fbeaf9592bdb8fbdd0a9ee14b2e3e00a54be4
lucasjord <lucas_hyland@hotmail.com>
2021-08-03 13:58:18 +1000

    fixed error in distribute

--- distribute.sh
+++ distribute.sh
@@ -4,12 +4,16 @@ cd /home/observer/correlations2/spiralssched/ && git pull
 
 export class=$(echo $1 | sed 's/.$//')
 
-echo "Newsmerd for slogit"
+echo "\n Newsmerd for slogit"
 rsync -Puv /home/observer/correlations2/spiralssched/vex/$1.vex observer@newsmerd:/vlbobs/lba/$class/$1.vex
-echo "Hobart12"
+echo "\n Hobart12"
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfshb:~/
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfs-2hb:/usr2/sched/
-echo "Katherine"
+echo "\n Katherine"
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfske:~/
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfs-2ke:/usr2/sched/
 
+echo "\n hex6"
+rsync -Puv /home/observer/correlations2/spiralssched/vex/$1.vex /home/observer/correlations2/$1/$1.vex
+
+echo "\n Done"
