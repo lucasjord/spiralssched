028aa0dde00aa7968ed41bde6fa7658765054067
lucasjord <lucas_hyland@hotmail.com>
2021-08-05 10:38:24 +1000

    Added Ceduna procedures to distribute and added x420d schedule

--- distribute.sh
+++ distribute.sh
@@ -6,10 +6,15 @@ export class=$(echo $1 | sed 's/.$//')
 
 echo ' '
 echo "##############################"
-echo "Newsmerd for slogit"
-# Copying vex file to newsmerd for slogit
-ssh observer@newsmerd mkdir /vlbobs/lba/$class
-scp /home/observer/correlations2/spiralssched/vex/$1.vex observer@newsmerd.phys.utas.edu.au:/vlbobs/lba/$class/$1.vex
+echo "Ceduna"
+# Copying vex file to Ceduna
+scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfscd:/usr2/sched/$1.vex
+# Removing old files
+ssh oper@pcfscd 'cd /usr2/sched; rm '$1cd.*
+# Drudging
+ssh oper@pcfscd 'cd /usr2/sched; echo -e "cd\n11\n19 16 1 1\n3\n0\n"|/usr2/fs/bin/drudg '$1.vex
+# Copying template vex over
+ssh oper@pcfscd 'cp /usr2/proc/spiralscd.prc /usr2/proc/'$1cd.prc
 
 echo "##############################"
 echo "Hobart12"
@@ -25,7 +30,7 @@ ssh oper@pcfshb /usr2/oper/AuscopeVLBI/fs/src/snp2flexbuff.py $1hb.snp
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfs-2hb:/usr2/sched/ 
 # Drudg vex file
 ssh oper@pcfs-2hb 'cd /usr2/sched/; rm '$1hb.snp
-ssh oper@pcfs-2hb 'cd /usr2/sched; echo -e "hb\n11\n19 16 1 1\n3\n\n0\n"|/usr2/fs/bin/drudg '$1.vex
+ssh oper@pcfs-2hb 'cd /usr2/sched; echo -e "hb\n11\n19 16 1 1\n3\n0\n"|/usr2/fs/bin/drudg '$1.vex
 # Copy template proc file to /usr2/proc
 ssh oper@pcfs-2hb 'cp /usr2/proc/spirals.prc /usr2/proc/'$1hb.prc
 
@@ -44,7 +49,7 @@ ssh oper@pcfske /usr2/oper/AuscopeVLBI/fs/src/snp2flexbuff.py $1ke.snp
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfs-2ke:/usr2/sched/ 
 # Drudg vex file
 ssh oper@pcfs-2ke 'cd /usr2/sched; rm '$1ke.snp
-ssh oper@pcfs-2ke 'cd /usr2/sched; echo -e "ke\n11\n19 16 1 1\n\n3\n0\n"|/usr2/fs/bin/drudg '$1.vex
+ssh oper@pcfs-2ke 'cd /usr2/sched; echo -e "ke\n11\n19 16 1 1\n3\n0\n"|/usr2/fs/bin/drudg '$1.vex
 # Copy template proc file to /usr2/proc
 ssh oper@pcfs-2ke 'cp /usr2/proc/spirals.prc /usr2/proc/'$1ke.prc
 
@@ -56,15 +61,15 @@ cp /home/observer/correlations2/spiralssched/vex/$1.vex /home/observer/correlati
 cd /home/observer/correlations2/$1/ && /home/observer/correlations2/corr_scripts/kludge_vex.sh $1
 
 # Making empty filelists
-touch hbX.filelist
-touch hbY.filelist
-touch keX.filelist
-touch keY.filelist
-touch cd.filelist
-touch wa.filelist
-touch ho.filelist
-touch wa.filelist
-touch yg.filelist
+touch /home/observer/correlations2/$1/hbX.filelist
+touch /home/observer/correlations2/$1/hbY.filelist
+touch /home/observer/correlations2/$1/keX.filelist
+touch /home/observer/correlations2/$1/keY.filelist
+touch /home/observer/correlations2/$1/cd.filelist
+touch /home/observer/correlations2/$1/wa.filelist
+touch /home/observer/correlations2/$1/ho.filelist
+touch /home/observer/correlations2/$1/wa.filelist
+touch /home/observer/correlations2/$1/yg.filelist
 
 echo ' '
 echo "##############################"
