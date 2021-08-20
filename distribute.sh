b03c42f598a6afc69532a6decd99b909a96959f7
lucasjord <lucas_hyland@hotmail.com>
2021-08-20 11:41:16 +1000

    Revert "Added start and current time to end, added EOF wraps around ssh"
    This reverts commit b49ea0aa2ee9339bd446ccd6c4f99d6e71b32482.

--- distribute.sh
+++ distribute.sh
@@ -39,16 +39,14 @@ echo "Ceduna"
 echo ' '
 # Copying vex file to Ceduna
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfscd:/usr2/sched/$1.vex
-ssh oper@pcfscd << EOF
-	# Removing old files
-	cd /usr2/sched; rm $1cd.*
-	# Drudging
-	cd /usr2/sched; echo -e "cd\n11\n19 16 1 1\n3\n0\n"|/usr2/fs/bin/drudg $1.vex
-	# Add source=stow
-	echo "source=stow" >> /usr2/sched/$1cd.snp
-	# Copying template vex over
-	cp /usr2/proc/spiralscd.prc /usr2/proc/$1cd.prc
-EOF
+# Removing old files
+ssh oper@pcfscd 'cd /usr2/sched; rm '$1cd.*
+# Drudging
+ssh oper@pcfscd 'cd /usr2/sched; echo -e "cd\n11\n19 16 1 1\n3\n0\n"|/usr2/fs/bin/drudg '$1.vex
+# Add source=stow
+ssh oper@pcfscd 'echo "source=stow" >> /usr2/sched/'$1cd.snp
+# Copying template vex over
+ssh oper@pcfscd 'cp /usr2/proc/spiralscd.prc /usr2/proc/'$1cd.prc
 
 echo ' '
 echo "############################################################"
@@ -56,61 +54,52 @@ echo "Hobart12"
 echo ' '
 # Copy vex to pcfshb home area
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfshb:~/
-ssh oper@pcfhb << EOF
-	# drudg the vex file there
-	rm $1hb.snp
-	echo -e "hb\n3\n0\n"|/usr2/fs/bin/drudg $1.vex
-	# Run the snp2flex script
-	/usr2/oper/AuscopeVLBI/fs/src/snp2flexbuff.py $1hb.snp
-EOF
+# drudg the vex file there
+ssh oper@pcfshb rm $1hb.snp
+ssh oper@pcfshb 'echo -e "hb\n3\n0\n"|/usr2/fs/bin/drudg '$1.vex
+# Run the snp2flex script
+ssh oper@pcfshb /usr2/oper/AuscopeVLBI/fs/src/snp2flexbuff.py $1hb.snp
 
 # Copy vex file over the pcfs-2hb sched area
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfs-2hb:/usr2/sched/ 
 # Drudg vex file
-ssh oper@pcf-2hb << EOF
-	rm /usr2/sched/$1hb.snp
-	cd /usr2/sched; echo -e "hb\n11\n19 16 1 1\n3\n0\n"|/usr2/fs/bin/drudg $1.vex
-	# Add source=stow
-	echo "source=stow" >> /usr2/sched/$1hb.snp
-	# Copy template proc file to /usr2/proc
-	cp /usr2/proc/spirals.prc /usr2/proc/$1hb.prc
-EOF
+ssh oper@pcfs-2hb 'cd /usr2/sched/; rm '$1hb.snp
+ssh oper@pcfs-2hb 'cd /usr2/sched; echo -e "hb\n11\n19 16 1 1\n3\n0\n"|/usr2/fs/bin/drudg '$1.vex
+# Add source=stow
+ssh oper@pcfshb 'echo "source=stow" >> /usr2/sched/'$1hb.snp
+# Copy template proc file to /usr2/proc
+ssh oper@pcfs-2hb 'cp /usr2/proc/spirals.prc /usr2/proc/'$1hb.prc
 
 echo ' '
 echo "############################################################"
 echo "Katherine"
 echo ' '
-
 # Copy vex to pcfske home area
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfske:~/
-ssh oper@pcfke << EOF
-	# drudg the vex file there
-	rm $1ke.snp
-	echo -e "ke\n3\n0\n"|/usr2/fs/bin/drudg $1.vex
-	# Run the snp2flex script
-	/usr2/oper/AuscopeVLBI/fs/src/snp2flexbuff.py $1ke.snp
-EOF
+# drudg the vex file there
+ssh oper@pcfske rm $1ke.snp
+ssh oper@pcfske 'echo -e "ke\n3\n0\n"|/usr2/fs/bin/drudg '$1.vex
+# Run the snp2flex script
+ssh oper@pcfske /usr2/oper/AuscopeVLBI/fs/src/snp2flexbuff.py $1ke.snp
 
 # Copy vex file over the pcfs-2ke sched area
 scp /home/observer/correlations2/spiralssched/vex/$1.vex oper@pcfs-2ke:/usr2/sched/ 
 # Drudg vex file
-ssh oper@pcfs-2ke << EOF
-	rm /usr2/sched/$1ke.snp
-	cd /usr2/sched; echo -e "ke\n11\n19 16 1 1\n3\n0\n"|/usr2/fs/bin/drudg $1.vex
-	# Add source=stow
-	echo "source=stow" >> /usr2/sched/$1ke.snp
-	# Copy template proc file to /usr2/proc
-	cp /usr2/proc/spirals.prc /usr2/proc/$1ke.prc
-EOF
+ssh oper@pcfs-2ke 'cd /usr2/sched; rm '$1ke.snp
+ssh oper@pcfs-2ke 'cd /usr2/sched; echo -e "ke\n11\n19 16 1 1\n3\n0\n"|/usr2/fs/bin/drudg '$1.vex
+# Add source=stow
+ssh oper@pcfs-2ke 'echo "source=stow" >> /usr2/sched/'$1ke.snp
+# Copy template proc file to /usr2/proc
+ssh oper@pcfs-2ke 'cp /usr2/proc/spirals.prc /usr2/proc/'$1ke.prc
 
 echo ' '
 echo "############################################################"
 echo "Setting up correlator"
 echo ' '
 mkdir -p /home/observer/correlations2/$1
-cp /home/observer/correlations2/template/threads /home/observer/correlations2/$1/threads
-cp /home/observer/correlations2/template/machines /home/observer/correlations2/$1/machines
-cp /home/observer/correlations2/template/template.v2d /home/observer/correlations2/$1/$1.v2d
+cp  /home/observer/correlations2/template/threads /home/observer/correlations2/$1/threads
+cp  /home/observer/correlations2/template/machines /home/observer/correlations2/$1/machines
+cp  /home/observer/correlations2/template/template.v2d /home/observer/correlations2/$1/$1.v2d
 cp /home/observer/correlations2/spiralssched/vex/$1.vex /home/observer/correlations2/$1/$1.vex
 cd /home/observer/correlations2/$1/ && /home/observer/correlations2/corr_scripts/kludge_vex.sh $1
 
@@ -134,7 +123,4 @@ touch /home/observer/correlations2/$1/yg.filelist
 echo ' '
 echo "##############################"
 echo "Done"
-echo ' '
-echo "Experiment starts $START"
-echo "Time is now $(date -u +%Y.%j.%H.%M.%S)"
 echo ' '
